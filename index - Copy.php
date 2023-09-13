65434d8ce9a53dfd0194a02f447382d63e20fa24
asrendon <asrendon94@gmail.com>
2023-09-13 18:08:38 -0400

    commit thru cli

--- index - Copy.php
+++
@@ -1,1 +0,0 @@
-<h2>first website with edits</h2>
---
+++ index.php
@@ -0,0 +1,9 @@
+<h2>first website with edits</h2>
+test
+<script>console.log("test console")</script>
+
+<?php
+echo "test";
+$test2 = "test2";
+echo $test2;
+?>
